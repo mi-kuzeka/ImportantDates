e0255cb17ee7669a358038d070fcd66629f048f7
mi-kuzeka <ckatyao@yahoo.com>
2022-05-26 12:34:31 +0500

    Update EventRecyclerViewAdapter

--- app/src/main/java/ru/startandroid/importantdates/presentation/eventlist/EventsRecyclerViewAdapter.java
+++ app/src/main/java/ru/startandroid/importantdates/presentation/eventlist/EventsRecyclerViewAdapter.java
@@ -8,18 +8,21 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import ru.startandroid.importantdates.R;
 import ru.startandroid.importantdates.core.domain.Event;
 
 import java.util.List;
+
 /**
  * {@link RecyclerView.Adapter} that can display a {@link Event}.
  */
 public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final List<Event> eventList;
-    private Activity activity;
+    private final Activity activity;
 
     private final int VIEW_TYPE_ITEM = 0;
     private final int VIEW_TYPE_LOADING = 1;
@@ -33,7 +36,7 @@ public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
      * Initialize the dataset of the Adapter.
      *
      * @param eventList List<Event> containing the data to populate views to be used
-     *                 by RecyclerView.
+     *                  by RecyclerView.
      */
     public EventsRecyclerViewAdapter(RecyclerView recyclerView,
                                      List<Event> eventList,
@@ -69,11 +72,34 @@ public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
      * (custom ViewHolder).
      */
     public static class EventViewHolder extends RecyclerView.ViewHolder {
+        private final TextView dayTextView;
+        private final TextView nameTextView;
+        private final TextView categoryTextView;
+        private final ImageView eventImageView;
 
         public EventViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            //TODO: find views for the event item
+            this.dayTextView = itemView.findViewById(R.id.day_text_view);
+            this.nameTextView = itemView.findViewById(R.id.name_text_view);
+            this.categoryTextView = itemView.findViewById(R.id.category_text_view);
+            this.eventImageView = itemView.findViewById(R.id.event_image_view);
+        }
+
+        public TextView getDayTextView() {
+            return dayTextView;
+        }
+
+        public TextView getNameTextView() {
+            return nameTextView;
+        }
+
+        public TextView getCategoryTextView() {
+            return categoryTextView;
+        }
+
+        public ImageView getEventImageView() {
+            return eventImageView;
         }
     }
 
@@ -91,9 +117,13 @@ public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
         this.onLoadMoreListener = onLoadMoreListener;
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     */
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == VIEW_TYPE_ITEM) {
+            // Create a new view, which defines the UI of the list item
             View view = LayoutInflater.from(activity)
                     .inflate(R.layout.event_item, parent, false);
             return new EventViewHolder(view);
@@ -105,20 +135,53 @@ public class EventsRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
         return null;
     }
 
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof EventViewHolder) {
+            // Get element from your dataset at this position and replace the
+            // contents of the view with that element
+            Event eventItem = this.eventList.get(position);
+            EventViewHolder eventViewHolder = (EventViewHolder) holder;
+            Activity mainActivity = this.activity;
 
+            eventViewHolder.itemView.setOnClickListener(view -> {
+                //TODO: open event editor/creator
+                //Intent intent = new Intent(mainActivity, )
+            });
+
+            eventViewHolder.getDayTextView().setText(eventItem.getDay());
+            eventViewHolder.getNameTextView().setText(eventItem.getName());
+            eventViewHolder.getCategoryTextView().setText(eventItem.getCategory().getName());
+            eventViewHolder.getEventImageView().setImageBitmap(eventItem.getBitmapImage());
+
+        } else if (holder instanceof LoadingViewHolder) {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
     }
 
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
     @Override
     public int getItemCount() {
-        return eventList.size();
+        return this.eventList == null ? 0 : this.eventList.size();
     }
 
-    public class EventsViewHolder extends RecyclerView.ViewHolder {
+    public void setLoaded() {
+        isLoading = false;
+    }
 
-        public EventsViewHolder(View view) {
-            super(view);
-        }
+    /**
+     * Clear all items in the adapter
+     */
+    public void clear() {
+        if (this.eventList == null) return;
+        int size = this.eventList.size();
+        this.eventList.clear();
+        notifyItemRangeRemoved(0, size);
     }
 }
