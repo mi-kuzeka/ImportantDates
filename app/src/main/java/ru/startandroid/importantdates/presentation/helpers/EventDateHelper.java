12fb07a011531d062cf3285f29a6b482f978286a
mi-kuzeka <ckatyao@yahoo.com>
2022-06-06 20:54:28 +0500

    Fix date parsing

--- app/src/main/java/ru/startandroid/importantdates/presentation/helpers/EventDateHelper.java
+++ app/src/main/java/ru/startandroid/importantdates/presentation/helpers/EventDateHelper.java
@@ -7,9 +7,6 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 import ru.startandroid.importantdates.R;
 import ru.startandroid.importantdates.core.domain.EventDate;
 
@@ -25,14 +22,10 @@ public class EventDateHelper {
         String fullDateFormatPattern =
                 context.getResources().getString(R.string.full_date_format);
         // DateFormat for full date
-        SimpleDateFormat fullDateFormat = new SimpleDateFormat(
-                fullDateFormatPattern,
-                Locale.getDefault());
-        // Don't try to parse non-existent date,like 20.45.2000
-        fullDateFormat.setLenient(false);
+        DateTimeFormatter fullDateFormat = DateTimeFormat.forPattern(fullDateFormatPattern);
 
         try {
-            date = new DateTime(fullDateFormat.parse(dateText));
+            date = new DateTime(fullDateFormat.parseDateTime(dateText));
             // If date is successfully parsed
             // and user input text length equals pattern length,
             // then date is valid
@@ -45,14 +38,10 @@ public class EventDateHelper {
         String shortDateFormatPattern =
                 context.getResources().getString(R.string.date_format_without_year);
         // DateFormat for date without year
-        SimpleDateFormat shortDateFormat = new SimpleDateFormat(
-                shortDateFormatPattern,
-                Locale.getDefault());
-        // Don't try to parse non-existent date,like 20.45
-        shortDateFormat.setLenient(false);
+        DateTimeFormatter shortDateFormat = DateTimeFormat.forPattern(shortDateFormatPattern);
 
         try {
-            date = new DateTime(shortDateFormat.parse(dateText));
+            date = new DateTime(shortDateFormat.parseDateTime(dateText));
             // If date is successfully parsed
             // and user input text length equals short pattern length,
             // then date is valid
